4f42d570c3126e72c775ed4c8e04955d6e9ed1fe
PinHao Bill Chen <pinhao1994@gmail.com>
2024-08-20 22:46:42 +0800

    Remove out-of-date files and refine the .sh to set launchpad to 7 x 9.

--- mac-launchpad-7x9.sh
+++ mac-launchpad-7x9.sh
@@ -1,1 +1,4 @@
-defaults write com.apple.dock springboard-columns -int 9;defaults write com.apple.dock springboard-rows -int 7;defaults write com.apple.dock ResetLaunchPad -bool TRUE;killall Dock
+defaults write com.apple.dock springboard-columns -int 9;
+defaults write com.apple.dock springboard-rows -int 7;
+defaults write com.apple.dock ResetLaunchPad -bool TRUE;
+killall Dock
